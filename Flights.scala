6af02f755c7bf18dbeb51f7e8f415204cf0d376f
LukasCJung <csejcw@naver.com>
2018-02-14 19:37:15 +0900

    Update Flights.scala

--- Flights.scala
+++ Flights.scala
@@ -5,7 +5,7 @@ import org.apache.spark.SparkConf;
 
 
 object Flights {
-    def main(args: Array[String]) {
+   def  main(args: Array[String]) {
       System.setProperty("hadoop.home.dir", "D:\\data\\hadoop-common-2.2.0-bin-master")
       val conf = new SparkConf().setAppName("SparkJoins").setMaster("local").set("spark.executor.memory","1g")
 
@@ -15,20 +15,32 @@ object Flights {
       val header = input.first()
       
       //RDD[Array[String]]
-      val historys = input.map(line => { line.split(",")}).filter( data => 
-        try{ 
+      val historys = input.map(line => { line.split(",")}).filter( arrDelayFilter )
+        .map(data => ( data(0) + "-" + String.format("%2s",data(1)) + "-" + String.format("%2s",data(2)) , 1))
+    
+      val count = historys.reduceByKey(_+_)
+      count.sortByKey(true, 1).foreach(data => println(data._1 + " : " + data._2) )
+           
+    println("Hello, world!")
+  }
+   
+   
+   def arrDelayFilter( data : Array[String] ) : Boolean =  {
+     try{ 
           val time = data(14).toInt
           time > 0
         } catch {
           case e : Exception => false
-        })
-        .map(data => ( data(0) + "-" + data(1) + "-" + String.format("%2s",data(2)) , 1))
-    
-      val count = historys.reduceByKey(_+_)
-      count.sortByKey(true, 1).foreach(data => println(data._1 + " : " + data._2) )
-//      count.foreach( data => println(data._1 + " : " + data._2) )
-           
-    println("Hello, world!")
-  }
-    
+        }
+   }
+   
+   def depDelayFilter( data : Array[String] ) : Boolean =  {
+     try{ 
+          val time = data(15).toInt
+          time > 0
+        } catch {
+          case e : Exception => false
+        }
+   }
+   
 }
